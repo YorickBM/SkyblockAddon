d256baca455ce29f29591a9328f17749552853ef
YorickBM <yorickblom@hotmail.com>
2023-07-17 22:43:36 +0200

    Version update

--- src/main/java/yorickbm/skyblockaddon/SkyblockAddon.java
+++ src/main/java/yorickbm/skyblockaddon/SkyblockAddon.java
@@ -36,7 +36,7 @@ public class SkyblockAddon {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "skyblockaddon";
-    public static final String VERSION = "5.0";
+    public static final String VERSION = "5.1";
 
     public static final float UI_SOUND_VOL = 0.5f;
     public static final float EFFECT_SOUND_VOL = 0.2f;
