0d24cf3dd5bc6880414b593c2f2f5091ba6a952d
Yorick Blom <yorickblom@hotmail.com>
2025-02-23 16:09:24 +0100

    Update DefaultEventHandler.java
    Add support for server variables in GUI title & items

--- src/main/java/yorickbm/guilibrary/events/DefaultEventHandler.java
+++ src/main/java/yorickbm/guilibrary/events/DefaultEventHandler.java
@@ -1,6 +1,7 @@
 package yorickbm.guilibrary.events;
 
 import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -9,13 +10,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import yorickbm.guilibrary.interfaces.GuiClickItemEvent;
 import yorickbm.guilibrary.util.Helper;
+import yorickbm.skyblockaddon.capabilities.providers.SkyblockAddonWorldProvider;
+import yorickbm.skyblockaddon.configs.SkyBlockAddonLanguage;
+import yorickbm.skyblockaddon.islands.Island;
+import yorickbm.skyblockaddon.util.UsernameCache;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class DefaultEventHandler {
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void itemRenderer(GuiDrawItemEvent event) {
@@ -32,6 +39,29 @@ public class DefaultEventHandler {
             put("%pagenum%", event.getHolder().getCurrentPage()+"");
             put("%maxpage%", event.getHolder().getMaxPage()+"");
         }};
+
+        //Server variables
+        MinecraftServer server = event.getHolder().getOwner().getServer();
+        if(server != null) {
+            replacements.put("%server_name%", server.getMotd().trim());
+            replacements.put("%server_ip%", server.getLocalIp().trim());
+            replacements.put("%server_port%", server.getPort() + "");
+            replacements.put("%max_players%", server.getMaxPlayers()+"");
+            replacements.put("%current_players%", server.getPlayerCount()+"");
+
+            replacements.put("%server_tps%", String.format("%.2f", server.getAverageTickTime()));
+            replacements.put("%server_difficulty%", server.getWorldData().getDifficulty().toString());
+            replacements.put("%server_mod_name%", server.getServerModName().trim());
+
+            // Server Uptime Calculation (Compact)
+            long seconds = server.getTickCount() / 20;
+            long hours = seconds / 3600, minutes = (seconds % 3600) / 60, sec = seconds % 60;
+            String serverUptime = (hours > 0) ? String.format("%02dh %02dm %02ds", hours, minutes, sec) :
+                    (minutes > 0) ? String.format("%02dm %02ds", minutes, sec) :
+                            String.format("%02ds", sec);
+
+            replacements.put("%server_uptime%", serverUptime);
+        }
 
         event.getItemStackHolder().setDisplayName(name.stream()
                 .map(component -> {
@@ -62,6 +92,46 @@ public class DefaultEventHandler {
         event.drawItem(); //Add item into inventory
         event.setResult(Event.Result.ALLOW);
     }
+    @SubscribeEvent
+    public void dynamicTitle(OpenMenuEvent event) {
+        if(event.isCanceled()) return; //Skip if canceled
+
+        List<TextComponent> title = event.getTitle();
+        Map<String, String> replacements = new HashMap<>();
+        MinecraftServer server = event.getTarget().getServer();
+
+        if(server != null) {
+            replacements.put("%server_name%", server.getMotd().trim());
+            replacements.put("%server_ip%", server.getLocalIp().trim());
+            replacements.put("%server_port%", server.getPort() + "");
+            replacements.put("%max_players%", server.getMaxPlayers()+"");
+            replacements.put("%current_players%", server.getPlayerCount()+"");
+
+            replacements.put("%server_tps%", String.format("%.2f", server.getAverageTickTime()));
+            replacements.put("%server_difficulty%", server.getWorldData().getDifficulty().toString());
+            replacements.put("%server_mod_name%", server.getServerModName().trim());
+
+            // Server Uptime Calculation (Compact)
+            long seconds = server.getTickCount() / 20;
+            long hours = seconds / 3600, minutes = (seconds % 3600) / 60, sec = seconds % 60;
+            String serverUptime = (hours > 0) ? String.format("%02dh %02dm %02ds", hours, minutes, sec) :
+                    (minutes > 0) ? String.format("%02dm %02ds", minutes, sec) :
+                            String.format("%02ds", sec);
+
+            replacements.put("%server_uptime%", serverUptime);
+        }
+
+        event.setTitle(title.stream()
+                .map(component -> {
+                    String modifiedText = replacements.entrySet().stream()
+                            .reduce(component.getString(), (text, entry) -> text.replace(entry.getKey(), entry.getValue()), (a, b) -> b);
+                    return new TextComponent(modifiedText).withStyle(component.getStyle());
+                })
+                .filter(c -> c instanceof TextComponent) // Ensure it's a TextComponent
+                .map(c -> (TextComponent) c) // Cast to TextComponent
+                .collect(Collectors.toList()));
+    }
+
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void fillerRenderer(GuiDrawFillerEvent event) {
@@ -99,7 +169,6 @@ public class DefaultEventHandler {
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void openGui(OpenMenuEvent event) {
         if(event.isCanceled()) return; //Skip if canceled
-
         event.setResult(event.getTarget().openMenu(event.getProvider()).isPresent() ? Event.Result.ALLOW : Event.Result.DENY);
     }
 
@@ -114,21 +183,31 @@ public class DefaultEventHandler {
     public void onNextPage(ItemNextPageEvent event)
     {
         if(event.getHolder().nextPage()) event.getHolder().update();
-        else event.setCanceled(true);
+        else event.setResult(Event.Result.DENY);
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onPrevPage(ItemPreviousPageEvent event)
     {
         if(event.getHolder().prevPage()) event.getHolder().update();
-        else event.setCanceled(true);
+        else event.setResult(Event.Result.DENY);
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onItemClick(GuiClickItemEvent event)
     {
-        if(!event.isCanceled()) Helper.playSongToPlayer(event.getTarget(), SoundEvents.NOTE_BLOCK_CHIME, Helper.UI_SUCCESS_VOL, 1f); //Send success sound notification of click
-        else Helper.playSongToPlayer(event.getTarget(), SoundEvents.NOTE_BLOCK_BASS, Helper.UI_SUCCESS_VOL, 1f); //Send failure sound notification of click
+        LOGGER.info("Determine sound to be played - {}", event.getResult().toString());
+
+        if(!event.getResult().equals(Event.Result.DENY)) {
+            LOGGER.info("Play success sound");
+
+            Helper.playSongToPlayer(event.getTarget(), SoundEvents.NOTE_BLOCK_CHIME, Helper.UI_SUCCESS_VOL, 1f); //Send success sound notification of click
+        }
+        else {
+            LOGGER.info("Play failure sound");
+
+            Helper.playSongToPlayer(event.getTarget(), SoundEvents.NOTE_BLOCK_BASS, Helper.UI_SUCCESS_VOL, 1f); //Send failure sound notification of click
+        }
 
     }
 }
