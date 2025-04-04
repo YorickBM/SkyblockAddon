e720b313c30541f02862b053404bee7ce932673b
Yorick Blom <yorickblom@hotmail.com>
2025-04-04 19:16:18 +0200

    Update SkyblockAddon.java

--- src/main/java/yorickbm/skyblockaddon/SkyblockAddon.java
+++ src/main/java/yorickbm/skyblockaddon/SkyblockAddon.java
@@ -51,7 +51,7 @@ public class SkyblockAddon {
     public static final UUID MOD_UUID = UUID.fromString("36916271-8ff4-483c-9379-bde032a01270");
     public static final UUID MOD_UUID2 = UUID.fromString("36916271-8ff4-483c-9379-bde032a01271");
     public static final String MOD_ID = "skyblockaddon";
-    public static final String VERSION = "7.0";
+    public static final String VERSION = "7.1";
 
     public static final float UI_SOUND_VOL = 0.5f;
     public static final float UI_SUCCESS_VOL = 3f;
