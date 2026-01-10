a0d5650c2e7504ee4a6cc7cfdd381673e293284f
Yorick Blom <yorickblom@hotmail.com>
2026-01-10 18:34:49 +0100

    Building GadgetFix

--- 1.18.2/src/main/java/yorickbm/skyblockaddon/mixins/BuildingGadgetsMixinConfig.java
+++ 1.18.2/src/main/java/yorickbm/skyblockaddon/mixins/BuildingGadgetsMixinConfig.java
@@ -1,6 +1,9 @@
 package yorickbm.skyblockaddon.mixins;
 
-import com.direwolf20.buildinggadgets.common.items.*;
+import com.direwolf20.buildinggadgets.common.items.GadgetBuilding;
+import com.direwolf20.buildinggadgets.common.items.GadgetCopyPaste;
+import com.direwolf20.buildinggadgets.common.items.GadgetDestruction;
+import com.direwolf20.buildinggadgets.common.items.GadgetExchanger;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerLevel;
@@ -9,13 +12,13 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import yorickbm.skyblockaddon.core.configs.SkyBlockAddonLanguage;
 import yorickbm.skyblockaddon.core.islands.Island;
-import yorickbm.skyblockaddon.core.permissions.EntityVerification;
 import yorickbm.skyblockaddon.islands.InteractionHandler;
 
 import java.util.concurrent.atomic.AtomicReference;
@@ -24,49 +27,19 @@ import java.util.concurrent.atomic.AtomicReference;
 public class BuildingGadgetsMixinConfig {
 
     @Inject(method = {"m_7203_"}, at = @At("HEAD"), cancellable = true)
-    private void onUse(ServerLevel world, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
+    private void onUse(Level world, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
         ItemStack stack = player.getItemInHand(hand);
 
-        String trigger;
-        if ((Object) this instanceof GadgetDestruction) {
-            trigger = "onBreakBlock";
-        } else if ((Object) this instanceof GadgetExchanger) {
-            if (!canUseGadget(player, "onPlaceBlock") || !canUseGadget(player, "onBreakBlock")) {
-                player.displayClientMessage(new TextComponent(SkyBlockAddonLanguage.getLocalizedString("toolbar.overlay.nothere")).withStyle(ChatFormatting.DARK_RED), true);
-                player.containerMenu.broadcastChanges();
-                cir.setReturnValue(InteractionResultHolder.success(stack));
-                return;
-            }
-            return;
-        } else {
-            trigger = "onPlaceBlock";
-        }
-
-        if (!canUseGadget(player, trigger)) {
-            player.displayClientMessage(new TextComponent(SkyBlockAddonLanguage.getLocalizedString("toolbar.overlay.nothere")).withStyle(ChatFormatting.DARK_RED), true);
-            player.containerMenu.broadcastChanges();
-            cir.setReturnValue(InteractionResultHolder.success(stack));
-        }
-    }
-
-    private static boolean canUseGadget(Player player, String trigger) {
         final AtomicReference<Island> standingOn = new AtomicReference<>();
-        final EntityVerification verification = InteractionHandler.verifyEntity(player, standingOn);
+        if(InteractionHandler.verifyEntity(player, standingOn).asBoolean()) return;
 
-        if (verification == EntityVerification.NOT_A_PLAYER) return true;
-        if (verification == EntityVerification.NOT_IN_OVERWORLD) return true;
-        if (verification == EntityVerification.IS_ADMIN) return true;
-        if (verification == EntityVerification.CAP_NOT_FOUND) return true;
-        if (verification == EntityVerification.IS_ISLAND_OWNER) return true;
-        if (verification == EntityVerification.NOT_ON_ISLAND) return true;
-
-        return !InteractionHandler.checkPlayerInteraction(
-                standingOn,
-                (ServerPlayer) player,
-                (ServerLevel) player.getLevel(),
-                player.getOnPos(),
-                player.getMainHandItem(),
-                trigger
-        );
+        if(
+                InteractionHandler.checkPlayerInteraction(standingOn, (ServerPlayer) player, (ServerLevel) player.getLevel(), player.getOnPos(), player.getMainHandItem(),  "onPlaceBlock")
+                ||
+                InteractionHandler.checkPlayerInteraction(standingOn, (ServerPlayer) player, (ServerLevel) player.getLevel(), player.getOnPos(), player.getMainHandItem(),  "onBreakBlock")
+        ) {
+            player.displayClientMessage(new TextComponent(SkyBlockAddonLanguage.getLocalizedString("toolbar.overlay.nothere")).withStyle(ChatFormatting.DARK_RED), true);
+            cir.setReturnValue(InteractionResultHolder.fail(stack));
+        }
     }
 }
--- 1.18.2/src/main/java/yorickbm/skyblockaddon/mixins/MixinConnector.java
+++ 1.18.2/src/main/java/yorickbm/skyblockaddon/mixins/MixinConnector.java
@@ -1,14 +1,19 @@
 package yorickbm.skyblockaddon.mixins;
 
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.connect.IMixinConnector;
 
 public class MixinConnector implements IMixinConnector {
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     @Override
     public void connect() {
         Mixins.addConfigurations("/mixins/skyblockaddon.mixin.json");
         Mixins.addConfiguration("/mixins/quark.mixin.json");
         Mixins.addConfiguration("/mixins/buildinggadgets.mixin.json");
+
+        LOGGER.info("Mixin configuration's loaded!");
     }
 }
